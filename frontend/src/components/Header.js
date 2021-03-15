57a7b6da8adf5ccf06569cb509e75b126e411e76
arjundubey-cr <arjundubeycr@gmail.com>
2021-03-15 22:38:01 +0530

    added drawer

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -7,6 +7,8 @@ import Button from '@material-ui/core/Button'
 import IconButton from '@material-ui/core/IconButton'
 import MenuIcon from '@material-ui/icons/Menu'
 
+import Drawer from './Drawer'
+
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
@@ -20,20 +22,14 @@ const useStyles = makeStyles((theme) => ({
   },
 }))
 
-export default function ButtonAppBar() {
+const Header = () => {
   const classes = useStyles()
 
   return (
     <div className={classes.root}>
       <AppBar position='static'>
         <Toolbar>
-          <IconButton
-            edge='start'
-            className={classes.menuButton}
-            color='inherit'
-            aria-label='menu'>
-            <MenuIcon />
-          </IconButton>
+          <Drawer />
           <Typography variant='h5' className={classes.title}>
             TnP | FOET
           </Typography>
@@ -43,3 +39,5 @@ export default function ButtonAppBar() {
     </div>
   )
 }
+
+export default Header
