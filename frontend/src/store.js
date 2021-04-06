90170fbb614e38e2cbccade6b91cb3e476fa8ab8
arjundubey-cr <arjundubeycr@gmail.com>
2021-04-06 20:38:13 +0530

    created user login reducer

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,8 +1,11 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import { userLoginReducer } from './reducers/userReducers'
 
-const reducer = combineReducers({})
+const reducer = combineReducers({
+  userLogin: userLoginReducer,
+})
 
 const initialState = {}
 
