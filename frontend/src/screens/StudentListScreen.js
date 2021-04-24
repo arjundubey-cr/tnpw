b8f37abc681fb284d37b52d1f9151e6abfeff822
arjundubey-cr <arjundubeycr@gmail.com>
2021-04-24 19:52:09 +0530

    react-table implemented

--- frontend/src/screens/StudentListScreen.js
+++ frontend/src/screens/StudentListScreen.js
@@ -1,51 +1,53 @@
 import React, { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import { LinkContainer } from 'react-router-bootstrap'
 import { useTable } from 'react-table'
-import { Button, Table } from 'react-bootstrap'
+import { Table } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
-import { ReactTable } from '../components/ReactTable'
 import { listUsers } from '../redux/userDetails/userDetailsAction'
+import { toastNotification } from '../components/ToastNotif'
 
-const StudentListScreen = () => {
+const StudentListScreen = ({ location, history }) => {
   const dispatch = useDispatch()
-  const userList = useSelector((state) => state.userList)
-  const { loading, error, users } = userList
   const [data, setData] = useState([])
   const [dataloading, setDataLoading] = useState(true)
+
+  const userList = useSelector((state) => state.userList)
+  const { loading, error, users } = userList
+
+  const userInfo = useSelector((state) => state.userInfo)
+  const redirect = location.search ? location.search.split('=')[1] : '/'
+
   useEffect(() => {
+    if (!userInfo || !userInfo.isAdmin) {
+      history.push(redirect)
+    }
     if (dataloading || users === undefined) {
       dispatch(listUsers())
       setDataLoading(false)
     } else {
       setData(users)
-      console.log(data)
     }
-  }, [dispatch, dataloading, users])
+  }, [dispatch, dataloading, users, data, history, redirect, userInfo])
 
   const columns = React.useMemo(
     () => [
       {
-        Header: 'Column 1',
+        Header: 'First Name',
         accessor: 'firstName',
       },
-
       {
-        Header: 'Column 2',
+        Header: 'Last Name',
         accessor: 'lastName',
+      },
+      {
+        Header: 'Email Address',
+        accessor: 'email',
       },
     ],
 
     []
   )
 
-  const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    rows,
-    prepareRow,
-  } = useTable({
+  const { getTableProps, headerGroups, rows, prepareRow } = useTable({
     columns,
     data,
   })
@@ -55,74 +57,36 @@ const StudentListScreen = () => {
       <h1>Users</h1>
       {loading ? (
         <h2>loading</h2>
+      ) : error ? (
+        toastNotification(error, 'error')
       ) : (
-        <table {...getTableProps()}>
+        <Table striped bordered hover size='sm' {...getTableProps()}>
           <thead>
-            {
-              // Loop over the header rows
-
-              headerGroups.map((headerGroup) => (
-                // Apply the header row props
-
-                <tr {...headerGroup.getHeaderGroupProps()}>
-                  {
-                    // Loop over the headers in each row
-
-                    headerGroup.headers.map((column) => (
-                      // Apply the header cell props
-
-                      <th {...column.getHeaderProps()}>
-                        {
-                          // Render the header
-
-                          column.render('Header')
-                        }
-                      </th>
-                    ))
-                  }
-                </tr>
-              ))
-            }
+            {headerGroups.map((headerGroup) => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column) => (
+                  <th {...column.getHeaderProps()}>
+                    {column.render('Header')}
+                  </th>
+                ))}
+              </tr>
+            ))}
           </thead>
-
-          {/* Apply the table body props */}
-
-          <tbody {...getTableBodyProps()}>
-            {
-              // Loop over the table rows
-
-              rows.map((row) => {
-                // Prepare the row for display
-
-                prepareRow(row)
-
-                return (
-                  // Apply the row props
-
-                  <tr {...row.getRowProps()}>
-                    {
-                      // Loop over the rows cells
-
-                      row.cells.map((cell) => {
-                        // Apply the cell props
-
-                        return (
-                          <td {...cell.getCellProps()}>
-                            {
-                              // Render the cell contents
-
-                              cell.render('Cell')
-                            }
-                          </td>
-                        )
-                      })
-                    }
-                  </tr>
-                )
-              })
-            }
+          <tbody>
+            {rows.map((row, i) => {
+              prepareRow(row)
+              return (
+                <tr {...row.getRowProps()}>
+                  {row.cells.map((cell) => {
+                    return (
+                      <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                    )
+                  })}
+                </tr>
+              )
+            })}
           </tbody>
-        </table>
+        </Table>
       )}
     </div>
   )
